595041c8256b49495f6e8678a2cdcc79cf9ddc89
leo <leovergarah@gmail.com>
2019-12-05 22:30:21 -0500

    (Updates) to routes & schema_script

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, session
+from flask import Flask, session, request
 import random
 from werkzeug.security import generate_password_hash, check_password_hash
 import sqlite3
@@ -6,20 +6,48 @@ import sqlite3
 app = Flask(__name__)
 conn = sqlite3.connect('blog.db', check_same_thread=False)
 
-@app.route('/')
-def root():
+@app.route('/posts')
+def get_posts():
+    #show all the blog posts
+    # query db for all posts 
     pass
 
-@app.route('/login')
-def home():
+@app.route('/posts/<id>', methods=['PUT'])
+def update_post(id):
+    #search for post
+    # update post here
+    #send back successful JSON response
     pass 
+
+@app.route('/posts', methods=['POST'])
+def insert_post():
+    #insert post
+    # send back successful JSON response
+    pass 
+
+@app.route('/posts/<id>', methods=['DELETE'])
+def delete_post(id):
+    # search for post
+    # delete post 
+    # send back successful JSON response 
 
 @app.route('/login', methods=['POST'])
 def admin_login():
+    # check if the user exists in the DB. 
+    # send back an error if password/user is incorrect
     pass 
 
-@app.route("/logout")
-def logout():
+@app.route('/signup', methods=['POST'])
+def user_signup():
+    # signup form 
+    # take values from form
+    # add them to db
+    # send success message JSON.
+    pass 
+
+@app.route('/logout/<id>')
+def logout(id):
+    # delete session here.
     pass 
 
 if __name__ == '__main__':
---
+++ create_tables.py
@@ -0,0 +1,15 @@
+import sqlite3
+
+def create_tables():
+    conn = sqlite3.connect('blog.db')
+    sql_file = open('schema.sql')
+    sql_tables = sql_file.read()
+
+    conn.executescript(sql_tables)
+    conn.commit()
+    conn.close()
+
+    sql_file.close()
+
+if __name__ == '__main__':
+    create_tables()
