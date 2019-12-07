f3cd3177fa2b767b27f7c8061aeaa87143163550
leo <leovergarah@gmail.com>
2019-12-06 22:56:54 -0500

    (Added) insert statements

--- create_tables.py
+++ create_tables.py
@@ -1,4 +1,5 @@
 import sqlite3
+from seed.seed_data import seed_user_data, seed_blog_posts_data
 
 def create_tables():
     conn = sqlite3.connect('blog.db')
@@ -11,5 +12,20 @@ def create_tables():
 
     sql_file.close()
 
+def insert_data():
+    conn = sqlite3.connect('blog.db')
+    cursor = conn.cursor()
+    insert_users = seed_user_data()
+    insert_blogs = seed_blog_posts_data()
+    print(insert_blogs)
+
+    cursor.executescript(insert_users)
+    cursor.executescript(insert_blogs)
+    conn.commit()
+    conn.close()
+
+
+
 if __name__ == '__main__':
     create_tables()
+    insert_data()
