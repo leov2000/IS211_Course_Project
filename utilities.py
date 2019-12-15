4e496c50b09f91a0a78bbfe624d84b43203dd2bd
Leo <leovergarah@gmail.com>
2019-12-14 21:35:08 -0500

    (Added) user utilities for verifying password, retrieving user, user entries and inserting user

--- utilities.py
+++ utilities.py
@@ -1,3 +1,5 @@
+from werkzeug.security import generate_password_hash, check_password_hash
+
 def query_user():
     """
     A query string that queries the student table
@@ -53,9 +55,59 @@ def query_topic(topic):
     
     return sql_query
 
+def get_user_entries(user):
+
+    sql_query = f"""
+    SELECT
+    posts.title AS 'TITLE',
+    posts.category AS CATEGORY,
+    posts.ishidden AS 'HIDDEN',
+    posts.published AS 'PUBLISHED',
+    posts.content AS 'CONTENT',
+    users.user_name AS 'USER'
+    FROM posts
+    JOIN users ON users.users_id = posts.users_id
+    WHERE users.user_name = '{user}'
+    ORDER BY strftime('%s', posts.published) DESC;
+    """
+
+    return sql_query
+
+def get_users(user):
+    sql_query = f"""
+    SELECT 
+    user_name as user,
+    users_password as password
+    FROM users
+    WHERE user = '{user}';
+    """
+
+    return sql_query
+
+def check_user_password_hash(result, entered):
+    user_tuple = result[0]
+    (_, hashed_password) = user_tuple
+    verify = check_password_hash(hashed_password, entered)
+
+    return verify
+
+def insert_new_user(user, password):
+    hashed_pw = generate_password_hash(password)
+
+    insert_user = f"""
+    INSERT INTO users
+    (user_name, users_password)    
+    VALUES
+    ('{user}','{hashed_pw}');
+    """
+    
+    return insert_user
+
 def transform_to_dict(val_list):
     key_config = ('title', 'category', 'isHidden', 'pub_date', 'content', 'user')
 
     return [
         dict(zip(key_config, val_tup)) for val_tup in val_list 
     ]
+
+
